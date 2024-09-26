92271d479549fa162dc000ca2d11c5bc453ac2cb
Caley R <crombout@my.bcit.ca>
2024-09-26 12:32:34 -0700

    Committing Lab 04 to Github

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Add more specific paths as needed
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust to match your source directories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
